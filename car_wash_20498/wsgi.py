18042eaa3c2ad10bf524725b768f16ef96da2b9f
Crowdbotics <team@crowdbotics.com>
2020-09-21 07:23:33 +0000

    Update models

--- car_wash_20498/wsgi.py
+++ car_wash_20498/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'car_wash_20498.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "car_wash_20498.settings")
 
 application = get_wsgi_application()
